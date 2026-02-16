8a9caa01f49916cc600fb88e724f940433bac39a
JustHousten <justin.elsner@outlook.de>
2026-02-16 14:46:01 +0100

    Phase 1 - Lektion 1: Variablen und Datentypen.

---
+++ phase1-grundlagen/variablen.py
@@ -0,0 +1,37 @@
+# Lektion 1: Variablen und Datentypen
+# Variablen sind Speicherplätze für Werte
+
+# Text (String)
+charakter_name = "Krieger"
+
+# Ganze Zahl (Integer)
+level = 1
+gold = 500
+
+# Kommazahl (Float)
+angriff_multiplikator= 1.5
+
+# Wahr/Falsch (Boolean)
+ist_eingeloggt = True
+
+# Ausgabe
+print(charakter_name)
+print(level)
+print(gold)
+print(angriff_multiplikator)
+print(ist_eingeloggt)
+
+# Variablen können verändert werden
+gold = gold + 100
+print(gold) # Was wird ausgegeben?
+# Ausgegeben wird [gold500 + 100 = 600]
+
+# Weiter ausprobieren
+# print(charakter_name + level)
+# Das geht nicht weil es zwei grundverschiedene Dinge sind
+# Stattdessen folgendes:
+print(charakter_name + str(level))
+
+print("Charakter: " + charakter_name)
+print("Level: " + str(level))
+print("Gold: " + str(gold))
